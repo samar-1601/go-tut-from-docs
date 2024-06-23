571a537e842ea6f2d2f841a5e857eef0ce2e1327
samar-1601 <samar.pp.singh@gmail.com>
2024-06-23 20:44:38 +0530

    initial commit

--- create-a-module/hello/hello.go
+++ create-a-module/hello/hello.go
@@ -14,10 +14,10 @@ func main() {
 	log.SetFlags(0)
 
 	// A slice of names.
-    names := []string{"Gladys", "Samantha", "Darrin"}
+	names := []string{"Gladys", "Samantha", "Darrin"}
 
-    // Request greeting messages for the names.
-    messages, err := greetings.Hellos(names)
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
 
 	// If an error was returned, print it to the console and
 	// exit the program.
